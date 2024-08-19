867938e14feba4f29acdc1d655a1793cdf766fc7
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-08-19 12:34:22 +0300

    Create onboarding_page.dart

---
+++ lib/pages/onboarding_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:furniture/components/my_button.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            flex: 8,
+            child: Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/onboarding.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          const Expanded(
+            flex: 4,
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+              child: Column(
+                children: [
+                  SizedBox(height: 30),
+                  Align(
+                    alignment:
+                        Alignment.centerLeft,
+                    child: Text(
+                      'Enjoy Your Online\nShopping.',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Browse through all categories and shop the best furniture for your dream house.',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          MyButton(
+            onTap: () {},
+            buttonText: 'Get Started',
+          ),
+        ],
+      ),
+    );
+  }
+}
