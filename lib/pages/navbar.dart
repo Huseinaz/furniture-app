0cdd3a0b70976691ffc5b09741b21903039b6825
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-08-19 13:55:37 +0300

    Create navbar.dart

---
+++ lib/pages/navbar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:furniture/pages/home_page.dart';
+
+class Navbar extends StatefulWidget {
+  const Navbar({super.key});
+
+  @override
+  State<Navbar> createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+  int myIndex = 0;
+  List<Widget> widgetList = const [
+    HomePage(),
+    HomePage(),
+    HomePage(),
+    HomePage(),
+    HomePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: widgetList[myIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showUnselectedLabels: true,
+        selectedItemColor: const Color.fromARGB(255, 29, 97, 124),
+        backgroundColor: Colors.white,
+        onTap: (index) {
+          setState(() {
+            myIndex = index;
+          });
+        },
+        currentIndex: myIndex,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite_border), label: 'Favorite'),
+          BottomNavigationBarItem(icon: Icon(Icons.qr_code_scanner), label: 'Scan'),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart_outlined), label: 'Cart'),
+          BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
