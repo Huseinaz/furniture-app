d370c9463c1ca42da491fa08629d0ca8856ab10d
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-08-19 13:55:51 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:furniture/pages/navbar.dart';
 import 'package:furniture/pages/onboarding_page.dart';
 
 void main() {
@@ -15,7 +16,8 @@ class MyApp extends StatelessWidget {
       home: const OnboardingPage(),
       routes: {
         '/onboarding': (context) => const OnboardingPage(),
-        },
+        '/home': (context) => const Navbar(),
+      },
     );
   }
 }
